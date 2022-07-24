414ea5259c39decea74b89a8be21f22ce1dffe0b
JuanKitu <juanmascruz@gmail.com>
2022-07-24 19:34:09 -0300

    main - fix: change the way of creating sequelize

--- src/database/database.ts
+++ src/database/database.ts
@@ -8,16 +8,19 @@ dotenv.config({ path: `${baseRouteENV}/config/.env` });
 if (!process.env.DB_PORT) {
   process.env.DB_PORT = '-1';
 }
-let sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT, 10),
-  storage: ':memory:',
-  models: [`${baseRoute}/models`],
-});
+let sequelize;
+if (process.env.NODE_ENV !== 'test') {
+  sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10),
+    storage: ':memory:',
+    models: [`${baseRoute}/models`],
+  });
+}
 
 if (process.env.NODE_ENV === 'test') {
   sequelize = new Sequelize({
